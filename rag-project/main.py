8e8b09f5beb797282273511e5813f9aa2ea1e535
Asif Ahmed Sahil <asifahmedsahil.12@gmail.com>
2026-04-28 20:38:02 +0600

    complete rag based app created with steamlit UI

--- rag-project/main.py
+++ rag-project/main.py
@@ -1,26 +1,76 @@
 from dotenv import load_dotenv
+from langchain_openai import OpenAIEmbeddings
 from langchain_mistralai import ChatMistralAI
-from langchain_community.document_loaders import TextLoader
+from langchain_community.vectorstores import Chroma
 from langchain_core.prompts import ChatPromptTemplate
 
 load_dotenv()
 
-data = TextLoader(r"D:\genai-projects\rag-project\document_loader\notes.txt")
+embedding_model = OpenAIEmbeddings()
 
-docs = data.load()
-print(docs)
+vector_store = Chroma(
+    persist_directory="chroma_db",
+    embedding_function=embedding_model
+)
 
-template = ChatPromptTemplate.from_messages(
+retriever =  vector_store.as_retriever(
+    search_type = "mmr",
+    search_kwargs = {
+        "k": 4,
+        "fetch_k":10,
+        "lambda_mult": 0.5
+    }
+)
+
+llm = ChatMistralAI(model="mistral-small-2506")
+
+# prompt template
+
+prompt = ChatPromptTemplate.from_messages(
     [
-        ("system","you are a ai summarizer who summarize the text."),
-        ("human","{data}")
+        (
+              "system",
+            """You are a helpful AI assistant.
+
+Use ONLY the provided context to answer the question.
+
+If the answer is not present in the context,
+say: "I
+"""
+        ),
+        (
+            "human",
+            """Context:
+{context}
+
+Question:
+{question}
+"""
+        )
     ]
 )
 
-model = ChatMistralAI(model="mistral-small-2506")
+print("Rag system created ")
 
-prompt = template.format_messages(data=docs[0].page_content)
+print("press 0 to exit ")
 
-result = model.invoke(prompt)
+while True:
+    query = input("You : ")
+    if query == "0":
+        break
 
-print(result.content)
+    docs = retriever.invoke(query)
+
+    context = "\n\n".join(
+        [doc.page_content for doc in docs]
+    )
+
+    final_prompt = prompt.invoke({
+        "context":context,
+        "question":query 
+    })
+
+    response = llm.invoke(final_prompt)
+
+    print(f"\n AI : {response.content}")
+
